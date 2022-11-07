f66fce9272b962e62a3519461b95a2cac2b7ab52
Pieter Bots <p.w.g.bots@tudelft.nl>
2022-11-07 09:35:40 +0100

    Updated extensions and teams

---
+++ presto/extension_data.py
@@ -0,0 +1,20 @@
+# workaround -- temporary; to be implemented with database objects
+# NOTE: extensions are specified in hours
+# NOTE: double underscores denote a dummy user (number must follow)
+ASSIGNMENT_DEADLINE_EXTENSIONS = {
+    'Full Estafette Title':
+    {
+        'username__1': 48, 
+        'username__2': 48,
+    },
+}
+
+REVIEW_DEADLINE_EXTENSIONS = {
+    'Full Estafette Title':
+    {
+        'oneuser': 48, 
+        'otheruser': 24,
+    },
+}
+
+
---
+++ presto/team_data.py
@@ -0,0 +1,11 @@
+# dict keys must be the full relay name
+# partner list holds tuples (lead user name, partner user name [, separation date-time])
+# NOTE: suffix __n indicates a dummy participant defined by an instructor
+PARTNER_LISTS = {
+    'Test estafette':
+    [
+      ('username__1', 'username__2'),  
+    ],
+}
+
+
